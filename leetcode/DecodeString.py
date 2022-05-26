0bf26238d3455f3243dc32c0bbfdd74c3e300618
seongwon.choi <scott.choi1993@gmail.com>
2022-05-27 01:47:56 +0900

    upload

--- leetcode/DecodeString.py
+++ leetcode/DecodeString.py
@@ -10,15 +10,15 @@ class Solution(object):
         if s[0].isdigit():
             while s[idx].isdigit():
                 idx += 1
-            return int(s[:idx]) * self.decodeString(s[idx + 1:self.find_pair_idx(s)]) + \ 
-                self.decodeString(s[self.find_pair_idx(s) + 1:])
-        else:
+            return int(s[:idx]) * self.decodeString(s[idx + 1:self.find_balanced_brackets(s)]) + \ 
+                self.decodeString(s[self.find_balanced_brackets(s) + 1:])
+        else: # s[0].isalpha() == True
             while s[idx].isalpha():
                 idx += 1
             return s[:idx] + self.decodeString(s[idx:])
 
 
-    def find_pair_idx(self, s):
+    def find_balanced_brackets(self, s):
         state = 0
         for idx in range(1,len(s)):
             if s[idx] == '[': state += 1
