1da94588d55606df3cd12c4ec3785aba576851a4
CSW <59173164+worldbrighteststar@users.noreply.github.com>
2022-07-29 15:21:51 +0900

    Update 2_실패율.py

--- programmers/kakao2019/2_실패율.py
+++ programmers/kakao2019/2_실패율.py
@@ -7,7 +7,7 @@ from collections import Counter
 def solution(N, stages):
     answer = {i : float(0) for i in range(1, N + 1)}
     stages = Counter(stages)
-    reach_count = stages.get(N+1) if stages.get(N+1) is not None else 0
+    reach_count = stages.get(N+1) if stages.get(N+1) is not None else 0 # count of completing all stages
     
     for i in range(N, 0, -1):
         if stages.get(i) != None:
