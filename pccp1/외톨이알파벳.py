7e75994e33bfd4a0bdbc6fead1dda603d9f4ff4a
Choi <scott.choi1993@gmail.com>
2023-03-27 15:22:10 +0900

    add problem1

---
+++ pccp1/외톨이알파벳.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+def solution(input_string):
+    alphaInfo = defaultdict(int)
+    
+    currentAlpha = ''
+    for alpha in input_string:
+        if alpha != currentAlpha: # new alphabet group
+            alphaInfo[alpha] += 1
+            currentAlpha = alpha # update current alphabet
+    
+    answer = [alpha for alpha in sorted(alphaInfo) if alphaInfo[alpha] > 1]
+
+    return ''.join(answer) if len(answer) > 0 else "N"
