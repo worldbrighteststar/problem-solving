6c48a4bd78144aa48258399dec8c5db98c3903c9
Choi <scott.choi1993@gmail.com>
2023-04-20 14:59:54 +0900

    add problem3

--- pccp1/유전법칙.py
+++ pccp1/유전법칙.py
@@ -4,30 +4,20 @@ def solution(queries):
     genDict = {1:'RR', 2:'Rr', 3:'Rr', 4:'rr'}
     
     def dfs(number, currentType, currentGen):
-
-        if currentGen <= 2:
+        
+        if currentType in ['RR', 'rr']: # it has to be a first condition
+            return currentType   
+        
+        if currentGen <= 2: # second condition
             if currentGen == 1: return 'Rr'
             else: return  genDict[number]
         
-        if currentType in ['RR', 'rr']:
-            return currentType        
-        
         totalNumOfChildren = 4 ** (currentGen - 1)
-        childOrder = ceil(number / totalNumOfChildren * 4)
+        childOrder = ceil(number / totalNumOfChildren * 4) 
         newNumber = number - (childOrder - 1) * totalNumOfChildren / 4
-        
+
         return dfs(newNumber, genDict[childOrder], currentGen - 1)
             
     answer = [dfs(query[1], 'Rr', query[0]) for query in queries]
         
     return answer
-
-
-
-
-
-
-
-
-d = [[[4,  4**2]]]
-print(solution(d[0]))
